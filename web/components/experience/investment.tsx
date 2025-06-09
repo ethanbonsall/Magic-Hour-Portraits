2947ba3b6dfc43ddd8c366b05af383d796da85a4
ethanbonsall <ethanbonsall50@gmail.com>
2025-06-08 20:54:21 -0400

    mobile fix

--- web/components/experience/investment.tsx
+++ web/components/experience/investment.tsx
@@ -1,15 +1,15 @@
 /* eslint-disable @next/next/no-img-element */
 const Investment = () => {
   return (
-    <div className="flex md:flex-row flex-col items-center justify-center text-center md:text-left h-screen md:p-0 bg-background-200">
-      <div className="md:w-[50%] w-full flex justify-center md:justify-end">
+    <div className="flex md:flex-row flex-col items-center justify-center text-center md:text-left min-h-screen bg-background-200">
+      <div className="w-full md:w-[50%] flex justify-center md:justify-end">
         <img
           src="assets/investment.jpg"
           alt="investment"
           className="h-[40lvh] md:h-[80lvh] w-auto object-cover my-4 md:mb-0"
-        />
+        ></img>
       </div>
-      <div className="w-full md:w-[50%] pl-0 md:pl-8 text-text space-y-8 p-8">
+      <div className="w-full md:w-[50%] pl-0 md:pl-8 text-text space-y-8">
         <div>
           <p className="text-6xl md:text-7xl xl:text-8xl font-thin">
             YOUR INVESTMENT
@@ -22,7 +22,7 @@ const Investment = () => {
         </p>
         <p className="text-xl ml-0 md:ml-16">Investment begins at $2,399</p>
         <button
-          className="p-2 px-8 bg-primary-300 text-black ml-0 md:ml-16 text-xl"
+          className="p-2 px-8 bg-primary-300 text-black ml-0 md:ml-16  text-xl"
           onClick={() => (window.location.href = "/contact")}
         >
           Inquire →
