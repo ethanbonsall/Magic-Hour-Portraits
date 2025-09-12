4bc53db52f1ee5bb6e2222a2f05d3dd5e6562eb7
ethanbonsall <ethanbonsall50@gmail.com>
2025-09-11 23:03:14 -0400

    Small fix on experience page

--- web/components/experience/description.tsx
+++ web/components/experience/description.tsx
@@ -1,13 +1,13 @@
 /* eslint-disable @next/next/no-img-element */
 const Description = () => {
   return (
-    <div className="flex flex-col md:flex-row min-h-screen bg-primary-100 justify-evenly align-middle py-8 px-4 md:p-0">
+    <div className="flex flex-col md:flex-row min-h-screen bg-background-200 justify-evenly align-middle py-8 px-4 md:p-0">
       <img
         src="/assets/home/left.jpg"
         alt="Wedding"
         className="h-[80lvh] w-auto object-cover rounded-lg shadow-lg self-center"
       />
-      <div className="flex flex-col bg-primary-100 md:bg-secondary-400 w-full md:w-[50dvw] text-text md:text-white my-16">
+      <div className="flex flex-col bg-background-200 md:bg-secondary-400 w-full md:w-[50dvw] text-text md:text-white my-16">
         <hr className="inline-block md:hidden border-text mx-2"></hr>
         <p className="m-5 lg:m-6 xl:m-8 text-4xl xl:text-5xl">
           Throughout my career, I&apos;ve had the privilege of capturing some of
